500ed7b2dec0c66fb4ea14f2db32f50279e67f4b
David Moseler <davidmoseler@protonmail.com>
2020-06-18 20:54:36 -0300

    Switch sqlalchemy to redis

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
 import jwt
 import os
+import redis
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-db = SQLAlchemy(app)
 secret = os.environ.get('SECRET_KEY')
+redis_client = redis.Redis.from_url(os.environ.get('REDIS_URL'))
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String)
-    password = db.Column(db.String)
-
-    def __repr__(self):
-        return f"User(name={self.name})"
-
-@app.route('/', methods=['POST'])
-def authenticate(json=None):
-    user = User.query.first()
-    encoded_jwt = jwt.encode({'name': user.name}, secret, algorithm='HS256')
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    username = request.form['username']
+    password = request.form['password']
+    if redis_client.hgetall(username):
+        if redis_client.hget(username, 'password').decode('UTF-8') == password:
+            encoded_jwt = jwt.encode({'username': username}, secret, algorithm='HS256')
+            return {
+                'ok': True,
+                'jwt': encoded_jwt.decode('UTF-8')
+            }
     return {
-        'jwt': encoded_jwt.decode('UTF-8')
+        'ok': False
     }
