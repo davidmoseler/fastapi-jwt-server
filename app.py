ea194d02231bba41771a5484daeb456e68f1cc87
David Moseler <davidmoseler@protonmail.com>
2020-06-18 19:39:40 -0300

    Basic authentication done

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import jwt
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 db = SQLAlchemy(app)
+secret = os.environ.get('SECRET_KEY')
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -15,15 +18,10 @@ class User(db.Model):
     def __repr__(self):
         return f"User(name={self.name})"
 
-db.create_all()
-
-user = User(name='fulano')
-db.session.add(user)
-db.session.commit()
-
 @app.route('/', methods=['POST'])
 def authenticate(json=None):
     user = User.query.first()
+    encoded_jwt = jwt.encode({'name': user.name}, secret, algorithm='HS256')
     return {
-        'name': user.name
+        'jwt': encoded_jwt.decode('UTF-8')
     }
