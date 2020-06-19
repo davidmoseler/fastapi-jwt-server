ee3c18bb283af042b2bf8f76c7422a95886e39db
David Moseler <davidmoseler@protonmail.com>
2020-06-18 21:05:53 -0300

    Add registration and encription

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import jwt
 import os
 import redis
+import bcrypt
 
 app = Flask(__name__)
 secret = os.environ.get('SECRET_KEY')
@@ -12,12 +13,36 @@ def authenticate():
     username = request.form['username']
     password = request.form['password']
     if redis_client.hgetall(username):
-        if redis_client.hget(username, 'password').decode('UTF-8') == password:
+        try:
+            checked = bcrypt.checkpw(password.encode('UTF-8'), redis_client.hget(username, 'password'))
+        except ValueError:
+            return {
+                'ok': False,
+                'error': 'Invalid username/password pair'
+            }
+        if checked:
             encoded_jwt = jwt.encode({'username': username}, secret, algorithm='HS256')
             return {
                 'ok': True,
                 'jwt': encoded_jwt.decode('UTF-8')
             }
     return {
-        'ok': False
+        'ok': False,
+        'error': 'Invalid username/password pair'
     }
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    if redis_client.hgetall(username):
+        return {
+            'ok': False,
+            'error': 'Username already registered'
+        }
+    else:
+        password_hash = bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt())
+        redis_client.hset(username, 'password', password_hash)
+        return {
+            'ok': True
+        }
